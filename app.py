cfa2890d761eb7cbfaa5dbe349a6dcefb469e748
cnchoudhari <108466081+cnchoudhari@users.noreply.github.com>
2023-05-06 10:28:11 +0000

    add some code

--- app.py
+++ app.py
@@ -3,9 +3,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 JOBS = [{
   'id': 1,
+  'title': ' Plant operator',
+  'location': ' Chandrapur ',
+  'salary': ' Rs.22,000'
+}, {
+  'id': 2,
+  'title': '  Welder',
+  'location': ' Chandrapur ',
+  'salary': ' Rs.15,000'
+}, {
+  'id': 3,
   'title': ' Weibgh Bridge Operater',
   'location': ' Chandrapur ',
-  'salary': ' Rs.10,000'
+  'salary': ' Rs.12,000'
+}, {
+  'id': 4,
+  'title': ' Maintenance technician',
+  'location': ' Chandrapur ',
+  'salary': ' Rs.20,000'
+}, {
+  'id': 5,
+  'title': ' Industrial electrician',
+  'location': ' Chandrapur ',
+  'salary': ' Rs.28,000'
 }]
 
 
