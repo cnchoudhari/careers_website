989682386fa6c2958bfdae0362f4f3bb3f9d498d
cnchoudhari <108466081+cnchoudhari@users.noreply.github.com>
2023-05-12 08:10:39 +0000

    added some file

--- database.py
+++ database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine, text
 import os
 
 connection_string = os.environ['CONNECTION_STRING']
-engine = create_engine(connection_string, echo=True)
+engine = create_engine(connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 
 def load_jobs_from_db():
@@ -12,3 +15,14 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(row._asdict())
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                          {"val": id})
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
